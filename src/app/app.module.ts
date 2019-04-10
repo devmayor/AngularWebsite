5943de01bcc6f199fd67c3e64b37ae1745a1c687
Afolabi Mayowa <afotrick2011@gmail.com>
2019-04-10 13:48:50 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { InformationComponent } from './components/information/information.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
+import { StartComponent } from './components/start/start.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    QuoteComponent,
+    FeaturesComponent,
+    InformationComponent,
+    FaqComponent,
+    TestimonialsComponent,
+    BenefitsComponent,
+    StartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
