5834e8803e214f079e7573498c8cc23c61835feb
Afolabi Mayowa <afotrick2011@gmail.com>
2019-04-11 12:27:41 +0100

    accordion done

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-
+accordion = [
+  { heading: 'What kind of vehicles will you buy?',
+    article: `At sevvicar we love all kinds of vehicles including
+             trucks, cars, vans, mini vans and Suv\'s. We want to
+              buy and year, make or model`,
+    active: true
+  },
+  { heading: 'What do i need to sell my car?',
+    article: `At sevvicar we love all kinds of vehicles including
+             trucks, cars, vans, mini vans and Suv\'s. We want to
+              buy and year, make or model`,
+    active: false
+  },
+  { heading: 'What do i need to buy a car?',
+    article: `At sevvicar we love all kinds of vehicles including
+             trucks, cars, vans, mini vans and Suv\'s. We want to
+              buy and year, make or model`,
+    active: false
+  }
+];
   constructor() { }
 
   ngOnInit() {
+  }
+  accordionChange(index) {
+    this.accordion[index].active = !this.accordion[index].active;
   }
 
 }
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
+public toggle = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleNav() {
+    this.toggle = !this.toggle;
+  }
 }
